7e0a3d7c81842dce95274c28bbed05f2ae8555ae
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2018-07-02 17:45:20 +0200

    Add files via upload

---
+++ CTRDemo.py
@@ -0,0 +1,11 @@
+import requests
+from collections import Counter
+
+RNJ = "https://www.gutenberg.org/files/1513/1513-0.txt"
+
+Result = requests.get(url = RNJ)
+
+Text = Counter([word for word in Result.text.replace("\r\n"," ").split(" ") if word ])
+
+print(Text.most_common(n = 5))
+print(Text['I'])
